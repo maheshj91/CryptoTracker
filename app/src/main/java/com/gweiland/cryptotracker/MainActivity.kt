9c124d71e8aa1e1a7cf44650a720610192413c72
Maheshwar Singh <singh.maheshwar91@gmail.com>
2023-11-16 19:34:50 +0530

    Layout designs

--- app/src/main/java/com/gweiland/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/gweiland/cryptotracker/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.gweiland.cryptotracker
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.parseColor("#00FFFFFF")))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.menu_notification -> {
+                showToast(getString(R.string.msg_pending_task))
+               true
+            }
+
+            R.id.menu_settings -> {
+                showToast(getString(R.string.msg_pending_task))
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }
